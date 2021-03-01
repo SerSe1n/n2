40758a5650ff75187ecbae28dceefe226c6b2704
SerSe1n <serse1n.sen@gmail.com>
2021-03-01 15:53:33 +0300

    First commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
+#asd
 
 class Window(QMainWindow):
     def __init__(self):
